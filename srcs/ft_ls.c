2bc5283256a0d35f775a07fc51402897787f7203
Benjamin PRUNEVIELLE <bprunevi@e1r7p17.42.fr>
2019-04-03 15:58:49 +0200

    added ft_ls.c

---
+++ srcs/ft_ls.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bprunevi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/03 15:43:25 by bprunevi          #+#    #+#             */
+/*   Updated: 2019/04/03 15:58:31 by bprunevi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int add_args(int args, const char *argv)
+{
+	int a;
+
+	a = 0;
+	while (argv[++a])
+	{
+		a += argv[a] & ARG_R;
+	}
+	return (args);
+}
+
+int ls(int attr, const char *path)
+{
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+	int args;
+
+	args = 0;
+	if (argc <= 1)
+		return (ls(args, argv[0]));
+	while (++i < argc)
+		if (argv[i][0] == '-')
+			add_args
+}
+
