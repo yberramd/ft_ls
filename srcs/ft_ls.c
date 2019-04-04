cae35053ce5b9327fdc6b86348f422b2a0eb3a7e
Benjamin PRUNEVIELLE <bprunevi@e1r11p23.42.fr>
2019-04-04 15:59:33 +0200

    push de pre re-orginasation des fichiers

--- srcs/ft_ls.c
+++ srcs/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: bprunevi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 15:43:25 by bprunevi          #+#    #+#             */
-/*   Updated: 2019/04/03 18:09:47 by bprunevi         ###   ########.fr       */
+/*   Updated: 2019/04/04 15:58:23 by bprunevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,13 @@
 #include <pwd.h>
 #include <grp.h>
 #include <time.h>
+#include <stdlib.h>
 
 typedef struct		s_dir
 {
 	struct dirent	*dirent;
-	struct s_dirent	*next;
+	struct stat		*file_info;
+	struct s_dir	*next;
 }					t_dir;
 
 /*Objectif actuel : creer une liste chainee pour pouvoir aficher les fichiers dans n'importe quel sens.*/
@@ -77,25 +79,103 @@ int	print_info(const char *path, int attr)
 	return (1);
 }
 
+t_dir *create_list(t_dir *first, t_dir *previous, DIR *dir)
+{
+	t_dir *current;
+	struct dirent *dirent;
+
+	if ((dirent = readdir(dir)))
+	   if (!(current = malloc(sizeof(t_dir))))
+		exit(printf("Failed Malloc") * 0 - 1);
+	if (!dirent)
+		return(first);
+	current->dirent = dirent;
+	current->file_info = NULL;
+	if (previous)
+		previous->next = current;
+	else
+		first = current;
+	return(create_list(first, current, dir));
+}
+
+int cmp (int attr, const char *path, t_dir *elem1, t_dir *elem2)
+{
+	int rtn;
+	rtn = 0;
+	if (attr & ARG_t)
+	{
+		rtn = elem1->file_info->st_mtimespec.tv_sec <=
+		   	elem2->file_info->st_mtimespec.tv_sec;
+	}
+	else
+	{
+		while (elem1->dirent->d_name[rtn] == elem2->dirent->d_name[rtn])
+			++rtn;
+		rtn = elem1->dirent->d_name[rtn] > elem2->dirent->d_name[rtn];
+	}
+	if (attr & ARG_r)
+		return (!rtn);
+	return (rtn);
+}
+
+int sort(const char *path, int attr, t_dir *list)
+{
+	struct dirent *tmp;
+	int witness = 0;
+	while (list->next)
+	{
+		if (cmp(attr, path, list, list->next))
+		{
+			tmp = list->dirent;
+			list->dirent = list->next->dirent;
+			list->next->dirent = tmp;
+			witness = 1;
+		}
+		list = list->next;
+	}
+	return(witness);
+}
+
+void show_list(t_dir *list) //DEBUG.
+{
+	printf("%s\n", list->dirent->d_name);
+	while ((list = list->next))
+		printf("%s\n", list->dirent->d_name);
+	printf("\033[0m");
+}
+
+void stat_my_list(char *path, t_dir *list) //Requires libft
+{
+	while (list)
+	{
+		char *newpath;
+		list->file_info = malloc(sizeof(struct stat));
+		newpath = ft_strjoin(path, list->dirent->d_name);
+		path(newpath, list->file_info);
+		free(newpath);
+		list = list->next;
+	}
+}
+
 int ls(int attr, const char *path)
 {
 	DIR *dir;
+	t_dir *list;
 
 	(void)attr;
 	if (!(dir = opendir(path)))
 		return(print_info(path, attr));
-	printf("Fichier ouvert !\n");
+	list = create_list(NULL, NULL, dir);
 	closedir(dir);
+	printf("\033[0;33m");
+	show_list(list);
+	while (sort(path, attr, list))
+		(void)list;
+	printf("\033[0;32m");
+	show_list(list);
 	return (0);
 }
 
-/*
-t_dir create_chain(DIR *dir)
-{
-	t_dir *first_elem;
-	return(first_elem);
-}
-*/
 
 int main(int argc, char **argv)
 {
